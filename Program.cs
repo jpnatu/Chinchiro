50fd58251b94e5bcc9980391780799553686d8db
jpnatu <jpnatu2569@gmail.com>
2023-05-27 14:13:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Chinchiro;
+
+public class Program
+{
+    Random rnd = new Random();
+
+    void Roll_Dice()
+    {
+        int[] dice = new int[] {/*rnd.Next(1, 7), rnd.Next(1, 7), rnd.Next(1, 7)*/1,2,3};
+        Console.WriteLine(dice);
+    }
+
+    //メインループ
+    public static void Main(string[] args)
+    {
+        Program chinchiro = new Program();
+    
+        while (true)
+        {
+            if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                chinchiro.Roll_Dice();
+            }
+
+        }
+
+    }
+}
