8ff8d3dbe28ff52ba8f33fc4e1eb7b0fa32150ef
jpnatu <jpnatu2569@gmail.com>
2023-05-28 20:32:13 +0900

    Random three dice

--- Program.cs
+++ Program.cs
@@ -6,8 +6,28 @@ public class Program
 
     void Roll_Dice()
     {
-        int[] dice = new int[] {/*rnd.Next(1, 7), rnd.Next(1, 7), rnd.Next(1, 7)*/1,2,3};
-        Console.WriteLine(dice);
+        var diceno = 0;
+        int[] dice = {rnd.Next(1, 7), rnd.Next(1, 7), rnd.Next(1, 7)};
+        foreach (int i in dice)
+        {
+            
+            Console.Write(i);
+
+            if (dice[0] != dice[1] && dice[0] != dice[2])
+            {
+                diceno = dice[0];
+            }
+            else if (dice[1] != dice[0] && dice[1] != dice[2])
+            {
+                diceno = dice[1];
+            }
+            else if (dice[2] != dice[0] && dice[2] != dice[1])
+            {
+                diceno = dice[2];
+            }
+
+        }
+
     }
 
     //メインループ
