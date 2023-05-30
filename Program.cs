8273df5d07b7b01d24af300715ebd1f6ca298352
jpnatu <jpnatu2569@gmail.com>
2023-05-30 10:39:51 +0900

    duplicate class

--- Program.cs
+++ Program.cs
@@ -2,47 +2,19 @@
 
 public class Program
 {
-    Random rnd = new Random();
 
-    void Roll_Dice()
-    {
-        var diceno = 0;
-        int[] dice = {rnd.Next(1, 7), rnd.Next(1, 7), rnd.Next(1, 7)};
-        foreach (int i in dice)
-        {
-            
-            Console.Write(i);
-
-            if (dice[0] != dice[1] && dice[0] != dice[2])
-            {
-                diceno = dice[0];
-            }
-            else if (dice[1] != dice[0] && dice[1] != dice[2])
-            {
-                diceno = dice[1];
-            }
-            else if (dice[2] != dice[0] && dice[2] != dice[1])
-            {
-                diceno = dice[2];
-            }
-
-        }
-
-    }
-
-    //メインループ
     public static void Main(string[] args)
     {
-        Program chinchiro = new Program();
+        RollDice rollDice = new RollDice();
     
         while (true)
         {
             if (Console.ReadKey().Key == ConsoleKey.Spacebar)
             {
-                chinchiro.Roll_Dice();
+                rollDice.Roll_Dice();
             }
-
         }
 
     }
+
 }
---
+++ RollDice.cs
@@ -0,0 +1,31 @@
+﻿namespace Chinchiro;
+internal class RollDice
+{
+    Random rnd = new Random();
+
+    public void Roll_Dice()
+    {
+        int[] dice = { rnd.Next(1, 7), rnd.Next(1, 7), rnd.Next(1, 7) };
+        foreach (int i in dice)
+        {
+            Console.Write(i);
+
+            int diceno;
+            if (dice[0] != dice[1] && dice[0] != dice[2])
+            {
+                diceno = dice[0];
+            }
+            else if (dice[1] != dice[0] && dice[1] != dice[2])
+            {
+                diceno = dice[1];
+            }
+            else if (dice[2] != dice[0] && dice[2] != dice[1])
+            {
+                diceno = dice[2];
+            }
+
+        }
+
+    }
+
+}
