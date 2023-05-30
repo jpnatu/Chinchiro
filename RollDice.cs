909794482269259675e84041b9a7b8077e4beefb
jpnatu <jpnatu2569@gmail.com>
2023-05-30 22:23:12 +0900

    make yaku-judge

--- RollDice.cs
+++ RollDice.cs
@@ -9,22 +9,53 @@ internal class RollDice
         foreach (int i in dice)
         {
             Console.Write(i);
-
-            int diceno;
-            if (dice[0] != dice[1] && dice[0] != dice[2])
-            {
-                diceno = dice[0];
-            }
-            else if (dice[1] != dice[0] && dice[1] != dice[2])
-            {
-                diceno = dice[1];
-            }
-            else if (dice[2] != dice[0] && dice[2] != dice[1])
-            {
-                diceno = dice[2];
-            }
-
         }
+
+        int diceno = ChkNum(dice);
+
+        if (diceno != 0)
+        {
+            Console.WriteLine($"Result is:{diceno}");
+        }
+        else if (diceno == 0)
+        {
+            Console.WriteLine($"Result is:None");
+        }
+
+    }
+
+    public int ChkNum(int[] dice)
+    {
+        var diceno = 0;
+
+        bool contains123 = dice.Contains(1) && dice.Contains(2) && dice.Contains(3);
+        bool contains456 = dice.Contains(4) && dice.Contains(5) && dice.Contains(6);
+
+        switch (dice[0])
+        {
+            case var a when a == dice[1] && a != dice[2]:
+                diceno = dice[2];
+            break;
+            case var b when b == dice[2] && b != dice[1]:
+                diceno = dice[1];
+            break;
+            case var c when c != dice[1] && dice[1] == dice[2]:
+                diceno = dice[0];
+            break;
+            default:
+                diceno = 0;
+            break;
+        }
+        if (contains456)
+        {
+            diceno = 456;
+        }
+        else if (contains123)
+        {
+            diceno = 123;
+        }
+
+        return diceno;
 
     }
 
